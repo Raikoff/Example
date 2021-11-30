68a691cbc66239fc3db4396c94d9eb391de89795
Raikoff <podchernin.ivan@gmail.com>
2021-11-30 10:51:21 +0300

    update21

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.Write("Введите пятизначное число: ");
+string a = Console.ReadLine();
+
+int num = int.Parse(a);
+
+if (num <= 9999 || num >= 999999)
+{
+Console.WriteLine("Не умеешь считать до пяти?");
+}
+   else if (int.Parse("" + num[0]) == int.Parse("" + num[5]) || int.Parse("" + num[1]) == int.Parse("" + num[3]))
+   {
+    Console.WriteLine("Число проверяеться на полидромом");
+   }
